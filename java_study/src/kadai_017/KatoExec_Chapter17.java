cb87e328f97534d133bfe46594af09c1fa182c37
hideto-fukushima <hide1998taka723@gmail.com>
2023-12-21 17:22:13 +0900

    課題第17章提出

---
+++ java_study/src/kadai_017/KatoExec_Chapter17.java
@@ -0,0 +1,20 @@
+package kadai_017;
+
+public class KatoExec_Chapter17 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
+		KatoIchiro_Chapter17 ichiro = new KatoIchiro_Chapter17();
+		KatoHanako_Chapter17 hanako = new KatoHanako_Chapter17();
+		
+		taro.setGivenName();
+		ichiro.setGivenName();
+		hanako.setGivenName();
+		
+		taro.execIntroduce();
+		ichiro.execIntroduce();
+		hanako.execIntroduce();
+	}
+
+}
---
+++ java_study/src/kadai_017/Kato_Chapter17.java
@@ -0,0 +1,20 @@
+package kadai_017;
+
+abstract public class Kato_Chapter17 {
+	public String familyName="加藤";
+	public String givenName="";
+	public String address="東京都中野区○×";
+	
+	
+	public void commonIntroduce() {
+		System.out.println("名前は"+this.familyName+this.givenName+"です");
+		System.out.println("住所は"+this.address+"です");
+	}
+	
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
